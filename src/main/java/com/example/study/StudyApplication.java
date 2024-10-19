fc33667110dca4591198e21fa2c9f4c0a58c4431
yongjun.kim <dydwns5236@gmail.com>
2024-10-19 14:55:34 +0900

    #1 コメントテスト

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -3,6 +3,7 @@ package com.example.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test
 @SpringBootApplication
 public class StudyApplication {
 
